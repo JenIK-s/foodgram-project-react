ab614a32bdd53a1167edea13c4f4a82df5dad182
Артём Емельянов <fertuv68@gmail.com>
2023-05-30 01:48:19 +0300

    TEST SERAI

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -23,7 +23,6 @@ class CurrentUserSerializer(UserSerializer):
             'username',
             'first_name',
             'last_name',
-            'password'
         )
 
     def get_is_subscribed(self, obj):
