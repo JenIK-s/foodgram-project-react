2a4efad5953056579ff1ea7cc8224f8d493fe353
Артём Емельянов <fertuv68@gmail.com>
2023-06-04 16:00:18 +0300

    me

--- backend/back/urls.py
+++ backend/back/urls.py
@@ -18,5 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls', namespace='api'))
+    path('api/', include('api.urls', namespace='api')),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken')),
 ]
