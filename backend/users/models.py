76de125e2b36e56b0f62dac9cb7b48296107b130
Артём Емельянов <fertuv68@gmail.com>
2023-06-09 17:45:48 +0300

    TEST RECIPES

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,47 +1,74 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     username = models.CharField(
         unique=True,
-        db_index=True,
-        max_length=100,
-        verbose_name='Логин',
-        help_text='Логин пользователя',
-    )
-    password = models.CharField(
-        max_length=255,
-        verbose_name='Пароль',
-        help_text='Пароль пользователя',
-    )
-    first_name = models.CharField(
-        max_length=50,
-        verbose_name='Имя пользователя',
-        help_text='Имя пользователя',
-    )
-    last_name = models.CharField(
-        max_length=50,
-        verbose_name='Фамилия',
-        help_text='Фамилия',
+        max_length=150,
+        verbose_name='Ваш логин'
     )
     email = models.EmailField(
         unique=True,
-        db_index=True,
-        max_length=50,
-        verbose_name='Электронная почта',
-        help_text='Адрес электронной почты',
+        max_length=150,
+        verbose_name='Ваш email'
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Ваше имя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Ваша фамилия'
     )
     is_subcribed = models.BooleanField(
         default=False,
         verbose_name='Подписка на автора',
         help_text='Отметьте для подписки на автора',
     )
-
     class Meta:
-        ordering = ['username']
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
-    def __str__(self):
-        return self.username
+# class User(AbstractUser):
+#     username = models.CharField(
+#         unique=True,
+#         db_index=True,
+#         max_length=100,
+#         verbose_name='Логин',
+#         help_text='Логин пользователя',
+#     )
+#     password = models.CharField(
+#         max_length=255,
+#         verbose_name='Пароль',
+#         help_text='Пароль пользователя',
+#     )
+#     first_name = models.CharField(
+#         max_length=50,
+#         verbose_name='Имя пользователя',
+#         help_text='Имя пользователя',
+#     )
+#     last_name = models.CharField(
+#         max_length=50,
+#         verbose_name='Фамилия',
+#         help_text='Фамилия',
+#     )
+#     email = models.EmailField(
+#         unique=True,
+#         db_index=True,
+#         max_length=50,
+#         verbose_name='Электронная почта',
+#         help_text='Адрес электронной почты',
+#     )
+#     is_subcribed = models.BooleanField(
+#         default=False,
+#         verbose_name='Подписка на автора',
+#         help_text='Отметьте для подписки на автора',
+#     )
+
+#     class Meta:
+#         ordering = ['username']
+#         verbose_name = 'Пользователь'
+#         verbose_name_plural = 'Пользователи'
+
+#     def __str__(self):
+#         return self.username
