464d37aded6f97e8455da011243aa34901256ead
Артём Емельянов <fertuv68@gmail.com>
2023-06-15 23:16:17 +0300

    maybe end?-

--- backend/back/settings.py
+++ backend/back/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', default='+5_1@)1-#35&9z0^hvn)f56es7pertw!6@*@pp9ab_3n*8l%$t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', default=True)
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['158.160.69.44']
 
